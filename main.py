97df0c3d2ab519f5368595cbdbfaacfa09475a1f
Nikolas-A-O-Schlagenhaufer <nikolas850@gmail.com>
2023-04-25 19:59:35 -0300

    Código Inicial

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+	dados = pd.read_csv('data/samples.csv', sep=',', header=0)
+	eixo = list(dados["'EEG Fp2'"].drop(0, axis=0).values)
+	eixo = [float(valor) for valor in eixo]
+	plt.plot(eixo)
+	plt.show()
+
+	
+if __name__ == '__main__':
+	main()
