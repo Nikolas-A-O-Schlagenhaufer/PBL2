846d9059b0e8a162da9226149d2007e61d506d7c
Nikolas-A-O-Schlagenhaufer <nikolas850@gmail.com>
2023-04-26 09:20:23 -0300

    Testes limpeza de dados

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
-	dados = pd.read_csv('data/samples.csv', sep=',', header=0)
-	eixo = list(dados["'EEG Fp2'"].drop(0, axis=0).values)
-	eixo = [float(valor) for valor in eixo]
-	plt.plot(eixo)
-	plt.show()
+	dados = pd.read_csv('data/Subject_001.csv', sep=',', header=0).drop(0, axis=0)
+	# print(dados.head())
+	dados.columns = [item[1:-1] for item in dados.columns]
+	dados.drop('EDF Annotations', axis=1, inplace=True)
+	# print(dados.head())
+	for column in list(dados.columns)[1:10]:
+		dados[column] = dados[column].astype(float)
+	# erros = []
+	# for i in range(len(dados)):
+		# try:
+			# float(dados['EEG C3'].iloc[i])
+		# except:
+			# erros.append(i)
+	# print(erros)
+	# print(dados['EEG C3'].iloc[erros[0]])
 
-	
 if __name__ == '__main__':
 	main()
