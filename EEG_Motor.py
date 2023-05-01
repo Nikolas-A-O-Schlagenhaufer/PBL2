8b63c06b08ef0a4fd30348e8169bda09ea63a7d2
Graunke <mateus.barroso1311@gmail.com>
2023-05-01 15:27:26 -0300

    sicronizando

--- EEG_Motor.py
+++ EEG_Motor.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 
 def main():
 	dados = pd.read_csv('data/EEG_Motor01.csv', sep=',', header=0)
-	eixo = list(dados["'Fc5.'"].drop(0, axis=0).values)
+	eixo = list(dados["'Fc6.'"].drop(0, axis=0).values)
 	eixo = [float(valor) for valor in eixo]
 	plt.plot(eixo)
 	plt.show()
+	
 	
 if __name__ == '__main__':
 	main()
